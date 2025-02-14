69a42cca3169861a3ee30d4c9670e694ec92d610
Levin-Alexey <levin.penza@yandex.ru>
2025-02-14 08:36:46 +0300

    the project is ready

---
+++ src/Book.java
@@ -0,0 +1,27 @@
+public class Book {
+    public String title;
+    public int releaseYear;
+    public Author author;
+    public int pages;
+
+    public Book(String title, int releaseYear, String name, String surname, int rating, int pages) {
+        this.title = title;
+        this.releaseYear = releaseYear;
+        this.author = new Author(name, surname, rating);
+        this.pages = pages;
+    }
+
+    public boolean isBig() {
+        return pages > 500;
+    }
+
+    public boolean matches(String word) {
+        return title.contains(word) || author.name.contains(word) || author.surname.contains(word);
+    }
+
+    public int estimatePrice() {
+        int cost = (int) Math.floor(((3 * pages) *  Math.sqrt(author.rating)));
+        return Math.max(cost, 250);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,10 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Book book = new Book("The Da Vinci Code", 2003, "Dan", "Brown", 4, 689);
+        System.out.println(book.isBig());
+        System.out.println(book.matches("Code"));
+        System.out.println(book.estimatePrice());
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
     }
 }
